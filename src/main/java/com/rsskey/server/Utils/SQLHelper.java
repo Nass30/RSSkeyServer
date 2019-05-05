1b9f872b1bcabef2bcb2f88b0faa31500a582fb8
Unknown <jdavid_nel@yahoo.fr>
2019-05-05 22:11:42 +0200

    SQLHelper
    add class to easily create request with database and dispose all object after

--- src/main/java/com/rsskey/server/Utils/SQLHelper.java
+++ src/main/java/com/rsskey/server/Utils/SQLHelper.java
@@ -1,5 +1,8 @@
 package com.rsskey.server.Utils;
 
+import com.rsskey.server.DAO.Exception.DAOException;
+import com.rsskey.server.Models.IModel;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -49,10 +52,116 @@ public class SQLHelper {
         return map;
     }
 
-    public static String stringMakerByAttr(String query, String attr)
+    public static ArrayList<Long> executeNonQuery(Connection connexion, String sql, boolean returnGeneratedKeys, Object... objets )
     {
-        query.replaceAll("TEST", attr);
-        // query.replaceAll("@ATTRVALUE", value);
-        return query;
+        PreparedStatement preparedStatement = null;
+        ResultSet valeursAutoGenerees = null;
+        ArrayList<Long> ids = new ArrayList<>();
+
+        try {
+            /* Récupération d'une connexion depuis la Factory */
+            preparedStatement = initialisationRequetePreparee( connexion, sql, returnGeneratedKeys, objets );
+            int statut = preparedStatement.executeUpdate();
+            /* Analyse du statut retourné par la requête d'insertion */
+            if ( statut == 0 ) {
+                throw new DAOException( "Échec de la création de l'utilisateur, aucune ligne ajoutée dans la table." );
+            }
+            /* Récupération de l'id auto-généré par la requête d'insertion */
+            valeursAutoGenerees = preparedStatement.getGeneratedKeys();
+            if ( valeursAutoGenerees.next() ) {
+                /* Puis initialisation de la propriété id du bean Utilisateur avec sa valeur */
+                ids.add( valeursAutoGenerees.getLong( 1 ) );
+            } /* else {
+                throw new DAOException( "Échec de la création de l'utilisateur en base, aucun ID auto-généré retourné." );
+            } */
+        } catch ( SQLException e ) {
+            throw new DAOException( e );
+        } finally {
+            closeCleanning( valeursAutoGenerees, preparedStatement, connexion );
+        }
+        return ids;
     }
+
+    public static Object executeQuery(Connection connexion, String sql, boolean returnGeneratedKeys, IModel model, Object... objets )
+    {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Object DBModel = null;
+
+        try {
+
+            preparedStatement = initialisationRequetePreparee( connexion, sql, returnGeneratedKeys, objets );
+            resultSet = preparedStatement.executeQuery();
+            /* Parcours de la ligne de données de l'éventuel ResulSet retourné */
+            if ( resultSet.next() ) {
+                DBModel = model.map( resultSet );
+                System.out.print("TEST SQL EXECUTION !!");
+                System.out.print(model.toString());
+            }
+        } catch ( SQLException e ) {
+            throw new DAOException( e );
+        } finally {
+            closeCleanning( resultSet, preparedStatement, connexion );
+        }
+        return DBModel;
+    }
+    /*
+     * Initialise la requête préparée basée sur la connexion passée en argument,
+     * avec la requête SQL et les objets donnés.
+     */
+    public static PreparedStatement initialisationRequetePreparee( Connection connexion, String sql, boolean returnGeneratedKeys, Object... objets ) throws SQLException {
+        System.out.print("passe par la !" + objets.length);
+        PreparedStatement preparedStatement = connexion.prepareStatement( sql );
+        for ( int i = 0; i < objets.length; i++ ) {
+            preparedStatement.setObject( i + 1, objets[i] );
+        }
+        return preparedStatement;
+    }
+
+    /* Fermeture silencieuse du resultset */
+    public static void closeCleanning( ResultSet resultSet ) {
+        if ( resultSet != null ) {
+            try {
+                resultSet.close();
+            } catch ( SQLException e ) {
+                System.out.println( "Échec de la fermeture du ResultSet : " + e.getMessage() );
+            }
+        }
+    }
+
+    /* Fermeture silencieuse du statement */
+    public static void closeCleanning( Statement statement ) {
+        if ( statement != null ) {
+            try {
+                statement.close();
+            } catch ( SQLException e ) {
+                System.out.println( "Échec de la fermeture du Statement : " + e.getMessage() );
+            }
+        }
+    }
+
+    /* Fermeture silencieuse de la connexion */
+    public static void closeCleanning( Connection connexion ) {
+        if ( connexion != null ) {
+            try {
+                connexion.close();
+            } catch ( SQLException e ) {
+                System.out.println( "Échec de la fermeture de la connexion : " + e.getMessage() );
+            }
+        }
+    }
+
+    /* Fermetures silencieuses du statement et de la connexion */
+    public static void closeCleanning( Statement statement, Connection connexion ) {
+        closeCleanning( statement );
+        closeCleanning( connexion );
+    }
+
+    /* Fermetures silencieuses du resultset, du statement et de la connexion */
+    public static void closeCleanning( ResultSet resultSet, Statement statement, Connection connexion ) {
+        closeCleanning( resultSet );
+        closeCleanning( statement );
+        closeCleanning( connexion );
+    }
+
 }
